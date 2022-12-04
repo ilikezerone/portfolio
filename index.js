20b11ff24c6a3927462dc787c5b431c25fd13bce
zerone <ilikezerone@gmail.com>
2022-12-04 16:00:04 +0100

    Website has been created

---
+++ index.js
@@ -0,0 +1,8 @@
+function hoverOn() {
+  document.getElementById('secret').innerHTML = 'this website.';
+  document.getElementById('secret').style.color = 'magenta';
+}
+function hoverOff() {
+  document.getElementById('secret').innerHTML = 'th...';
+  document.getElementById('secret').style.color = 'lightblue';
+}
